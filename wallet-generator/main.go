500fcd7af44d58ea80fb25e8e60ff3fc95aeeb55
0fatih <fatihfurkn@gmail.com>
2021-09-17 09:59:09 +0300

    generate wallet

---
+++ wallet-generator/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"crypto/ecdsa"
+	"fmt"
+	"log"
+
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/crypto"
+)
+
+func main() {
+	privateKey, err := crypto.GenerateKey()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	privateKeyBytes := crypto.FromECDSA(privateKey)
+	fmt.Println("Private Key: ", hexutil.Encode(privateKeyBytes[2:]))
+
+	publicKey := privateKey.Public()
+	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+	if !ok {
+		log.Fatal("cannot assert type: publicKey is not of type *ecdsa.PublicKey")
+	}
+
+	publicKeyBytes := crypto.FromECDSAPub(publicKeyECDSA)
+	fmt.Println("\nPublic Key:", hexutil.Encode(publicKeyBytes[4:]))
+
+	address := crypto.PubkeyToAddress(*publicKeyECDSA).Hex()
+	fmt.Println("\nAddress: ", address)
+
+}
