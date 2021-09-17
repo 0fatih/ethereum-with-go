f3c4319b083a2ae9665330cdf8be34e676c8f8d5
0fatih <fatihfurkn@gmail.com>
2021-09-17 09:34:25 +0300

    some fixes

--- erc20Token/main.go
+++ erc20Token/main.go
@@ -10,10 +10,12 @@ import (
 
 func main() {
 	start := time.Now()
+
 	if len(os.Args) < 2 {
 		fmt.Println("Enter an address")
 		os.Exit(1)
 	}
+
 	fetch.FetchBalances(os.Args[1])
 	elapsed := time.Since(start)
 	fmt.Println(fmt.Sprintf("Finished in %s", elapsed))
